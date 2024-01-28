2db7451592709c741619a7b2f984c3e5a7774a8b
David Cohen <deiv464@gmail.com>
2024-01-28 22:37:31 +0200

    const values for pygame

---
+++ const.py
@@ -0,0 +1,15 @@
+WIN_WIDTH, WIN_HEIGHT = 680, 640
+DIMENSION = 8
+# The size of each squares in the board
+SQ_SIZE = (WIN_WIDTH // DIMENSION, WIN_HEIGHT // DIMENSION)
+FPS = 30
+
+# The image names of the chess pieces
+PIECES = ['black_r', 'black_n', 'black_b', 'black_q', 'black_k', 'black_p',
+          'white_r', 'white_n', 'white_b', 'white_q', 'white_k', 'white_p']
+
+# Colors
+WHITE = (255, 255, 255)
+GRAY = (80, 80, 80)
+BLUE = (0, 0, 128)
+GREEN = (0, 128, 0)
