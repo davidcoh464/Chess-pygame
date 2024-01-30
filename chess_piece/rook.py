6b4c06b5a992cf08ca25e5da1779d1b6d8f5fdbd
David Cohen <deiv464@gmail.com>
2024-01-31 00:30:29 +0200

    add move counter for castle

--- chess_piece/rook.py
+++ chess_piece/rook.py
@@ -7,9 +7,19 @@ class Rook(Piece):
     def __init__(self, is_white: bool, pos: Tuple[int, int]):
         super().__init__('r', is_white, pos)
         self._straight_move = StraightMove(is_white)
+        self._move_counter = 0
 
     def get_attack_moves(self, board: List[List[Optional[Piece]]]) -> List[Tuple[int, int]]:
         return self._straight_move.get_attack_moves(board, pos=self.get_position())
 
     def get_peace_moves(self, board: List[List[Optional[Piece]]]) -> List[Tuple[int, int]]:
         return self._straight_move.get_peace_moves(board, pos=self.get_position())
+
+    def has_moved(self) -> bool:
+        return self._move_counter != 0
+
+    def increase_moves_counter(self):
+        self._move_counter += 1
+
+    def decrease_moves_counter(self):
+        self._move_counter -= 1
