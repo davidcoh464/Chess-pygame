01d3f24fa18ae24be67d092592c513fad9c09f28
David Cohen <deiv464@gmail.com>
2024-04-18 12:11:17 +0300

    add chess board colors plate

--- const.py
+++ const.py
@@ -1,4 +1,4 @@
-WIN_WIDTH, WIN_HEIGHT = 680, 640
+WIN_WIDTH, WIN_HEIGHT = 704, 672
 DIMENSION = 8
 # The size of each squares in the board
 SQ_SIZE = (WIN_WIDTH // DIMENSION, WIN_HEIGHT // DIMENSION)
@@ -9,7 +9,16 @@ PIECES = ['black_r', 'black_n', 'black_b', 'black_q', 'black_k', 'black_p',
           'white_r', 'white_n', 'white_b', 'white_q', 'white_k', 'white_p']
 
 # Colors
-WHITE = (255, 255, 255)
-GRAY = (80, 80, 80)
+WHITE = (240, 232, 223)
 BLUE = (0, 0, 128)
 GREEN = (0, 128, 0)
+
+LIME_GREEN = (50, 150, 50)
+DARK_GRAY = (70, 70, 70)
+GREEN_GRAY = (50, 100, 80)
+BLUE_GRAY = (21, 111, 121)
+RED = (180, 22, 28)
+BROWN = (139, 69, 19)
+OLIVE = (128, 128, 0)
+DARK_PURPLE = (160, 0, 210)
+ORANGE = (180, 130, 20)
