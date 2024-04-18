5843ac797309b3dc88cee68131843298c8abe862
David Cohen <deiv464@gmail.com>
2024-04-18 12:01:11 +0300

    positional scoring foreach piece

--- chess_ai/board_evaluation.py
+++ chess_ai/board_evaluation.py
@@ -1,8 +1,86 @@
-from chess_piece import Piece
+from typing import List, Dict
 from chess_board import ChessBoard
+from chess_piece import Piece
 
-CHECKMATE = 1000  # The score for checkmate.
+CHECKMATE = 100000  # The score for checkmate.
 STALEMATE = 0  # The score for stalemate.
+
+PAWN_SCORE = [
+    [8, 8, 8, 8, 8, 8, 8, 8],
+    [8, 8, 8, 8, 8, 8, 8, 8],
+    [5, 6, 7, 7, 7, 7, 6, 5],
+    [2, 3, 3, 5, 5, 3, 3, 2],
+    [1, 2, 3, 4, 4, 3, 2, 1],
+    [1, 1, 3, 4, 4, 3, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+KNIGHT_SCORE = [
+    [-10, -8, -6, -6, -6, -6, -8, -10],
+    [-8, -4, 0, 0, 0, 0, -4, -8],
+    [-6, 0, 4, 6, 6, 4, 0, -6],
+    [-6, 2, 6, 8, 8, 6, 2, -6],
+    [-6, 0, 6, 8, 8, 6, 0, -6],
+    [-6, 2, 4, 6, 6, 4, 2, -6],
+    [-8, -4, 0, 2, 2, 0, -4, -8],
+    [-10, -8, -6, -6, -6, -6, -8, -10]
+]
+
+BISHOP_SCORE = [
+    [5, 0, 0, 0, 0, 0, 0, 5],
+    [0, 5, 0, 0, 0, 0, 5, 0],
+    [0, 0, 10, 5, 5, 10, 0, 0],
+    [0, 0, 10, 15, 15, 10, 0, 0],
+    [0, 0, 10, 15, 15, 10, 0, 0],
+    [0, 0, 10, 5, 5, 10, 0, 0],
+    [0, 5, 0, 0, 0, 0, 5, 0],
+    [5, 0, 0, 0, 0, 0, 0, 5]
+]
+
+ROOK_SCORE = [
+    [0, 0, 4, 10, 10, 4, 0, 0],
+    [4, 4, 4, 10, 10, 4, 4, 4],
+    [1, 1, 2, 5, 5, 2, 1, 1],
+    [1, 2, 3, 10, 10, 3, 2, 1],
+    [1, 2, 3, 10, 10, 3, 2, 1],
+    [1, 1, 2, 5, 5, 2, 1, 1],
+    [4, 4, 4, 10, 10, 4, 4, 4],
+    [0, 0, 4, 10, 10, 4, 0, 0]
+]
+
+QUEEN_SCORE = [
+    [-5, -5, -5, -5, -5, -5, -5, -5],
+    [-5, 0, 5, 5, 5, 5, 0, -5],
+    [-5, 0, 5, 5, 5, 5, 0, -5],
+    [-5, 0, 5, 5, 5, 5, 0, -5],
+    [-5, 0, 5, 5, 5, 5, 0, -5],
+    [-5, 0, 5, 5, 5, 5, 0, -5],
+    [-5, 0, 5, 5, 5, 5, 0, -5],
+    [-5, -5, -5, -5, -5, -5, -5, -5]
+]
+
+KING_SCORE = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 5, 5, 5, 5, 0, 0],
+    [0, 5, 5, 10, 10, 5, 5, 0],
+    [0, 5, 10, 10, 10, 10, 5, 0],
+    [0, 5, 10, 10, 10, 10, 5, 0],
+    [0, 0, 5, 10, 10, 5, 0, 0],
+    [0, 5, 5, -5, -5, 5, 5, 0],
+    [0, 0, 10, 0, 0, 0, 10, 0]
+]
+
+PIECE_VALUE: Dict[str, int] = {"k": 0, "q": 900, "r": 500, "b": 310, "n": 300, "p": 100}
+
+POSITION_SCORE: Dict[str, List[List[int]]] = {
+    "n": KNIGHT_SCORE,
+    "b": BISHOP_SCORE,
+    "q": QUEEN_SCORE,
+    "r": ROOK_SCORE,
+    "p": PAWN_SCORE,
+    "k": KING_SCORE
+}
 
 
 class Evaluation:
@@ -12,7 +90,6 @@ class Evaluation:
     This class provides methods to evaluate the current state of a chessboard,
     assigning scores based on various factors such as piece values and game end status.
     Attributes:
-        _piece_values (dict): A dictionary mapping piece names to their corresponding values.
         _rows (tuple): A tuple representing the range of rows on the chessboard.
         _board (ChessBoard): The chessboard instance to be evaluated.
     """
@@ -23,14 +100,11 @@ class Evaluation:
         Args:
             board (ChessBoard): The chessboard instance to be evaluated.
         """
-        self._piece_values = {"k": 0, "q": 10, "r": 5, "b": 3, "n": 3, "p": 1}
         self._rows = tuple(range(8))
         self._board = board
 
     def evaluate_board(self):
         """
-        Evaluate the current state of the chessboard.
-
         Evaluates the current state of the chessboard and returns
         an evaluation score. The score represents the advantage of the current
         position for the white player. Positive scores indicate an advantage
@@ -53,19 +127,22 @@ class Evaluation:
                 piece = self._board.get_piece((row, col))
                 if piece:
                     if piece.is_white():
-                        evaluation_score += self.evaluate_piece(piece)
+                        evaluation_score += Evaluation.evaluate_piece(piece, row, col)
                     else:
-                        evaluation_score -= self.evaluate_piece(piece)
+                        evaluation_score -= Evaluation.evaluate_piece(piece, 7 - row, col)
         return evaluation_score
 
-    def evaluate_piece(self, piece: Piece):
+    @staticmethod
+    def evaluate_piece(piece: Piece, row, col):
         """
         Evaluate the value of a chess piece.
 
         This method evaluates the value of a given chess piece based on its type.
         Args:
             piece (Piece): The chess piece to be evaluated.
+            row: (int) The piece row
+            col: (int) The piece column
         Returns:
             int: The value of the given chess piece.
         """
-        return self._piece_values.get(piece.get_name())
+        return PIECE_VALUE[piece.get_name()] + POSITION_SCORE[piece.get_name()][row][col]
